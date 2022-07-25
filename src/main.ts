8e8841ad403258e75375911b3ebf7c5e4dbf7b1e
mengjie.zhang <mengjie.zhang@easystack.cn>
2022-07-25 15:40:11 +0800

    Swagger 文档配置

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { generateDocument } from './doc';
+declare const module: any;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // 创建文档
+  generateDocument(app);
+
+
+
+  // 添加热更新
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
+
   await app.listen(3001);
 }
 bootstrap();
