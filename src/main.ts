563244beefef8618696feed240db92fba948aa3f
mengjie.zhang <mengjie.zhang@easystack.cn>
2022-07-25 15:49:40 +0800

    接口版本化管理

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { VersioningType, VERSION_NEUTRAL } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { generateDocument } from './doc';
@@ -5,10 +6,14 @@ declare const module: any;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // 接口版本化管理 支持多个版本
+  app.enableVersioning({
+    defaultVersion: [VERSION_NEUTRAL, '1', '2'],
+    type: VersioningType.URI,
+  });
+
   // 创建文档
   generateDocument(app);
-
-
 
   // 添加热更新
   if (module.hot) {
